0efd9519d85acce947ad20440a595a8f1b7e5f14
Daniel Freitas <99812555+Daniel-Freitas0@users.noreply.github.com>
2024-03-14 11:48:42 -0300

    versão 1

---
+++ main.js
@@ -0,0 +1,77 @@
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const cron = require('node-cron');
+
+const groupChatId = '120363241557953097@g.us';
+let reminder = null;
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+		args: ['--no-sandbox']
+	}
+});
+
+// imprime o qr code no console
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+// quando o cliente está pronto
+client.on('ready', () => {
+    console.log('Client is ready!');
+    // manda a mensagem todo dia às 21h
+    cron.schedule('57 21 * * *', () => {
+        sendMessage();
+    }, {
+        timezone: 'America/Sao_Paulo'
+    });
+    // verifica se a mensagem foi respondida todo dia às 22h
+    cron.schedule('0 22 * * *', () => {
+        checkAnswer();
+    }, {
+        timezone: 'America/Sao_Paulo'
+    });
+});
+
+// executado quando uma mensagem é recebida
+client.on('message', async (msg) => {
+    if (msg.from == groupChatId && msg.body.toLowerCase() === 'sim') {
+        respondeu = true;
+        if (reminder) {
+            reminder.stop();
+            reminder = null;
+        }
+        await client.sendMessage(groupChatId, 'Que bom! 😊');
+    }
+    console.log('Mensagem recebida de:', msg.from);
+});
+
+// manda a mensagem
+async function sendMessage() {
+    respondeu = false;
+    const message = 'Oi, já lavo o fedido hoje? 🤔';
+
+    try {
+        await client.sendMessage(groupChatId, message);
+        console.log('Message sent:', message);
+    } catch (err) {
+        console.error('Error sending message:', err);
+    }
+}
+
+// verifica se a mensagem foi respondida. se não, seta um lembrete a cada hora
+async function checkAnswer() {
+    if (!respondeu) {
+        reminder = cron.schedule('0 * * * *', () => {
+            sendReminder();
+        });
+    }
+}
+
+// manda o lembrete
+async function sendReminder() {
+    await client.sendMessage(groupChatId, 'Responde ai, já lavo o fedido? 🤔');
+}
+
+client.initialize();
